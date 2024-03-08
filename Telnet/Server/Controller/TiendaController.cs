4038df8c7c1ee06b07e10e50d4a9674535f282cf
Hermenegildo <meredossantos@outlook.com>
2024-03-08 17:25:22 -0600

    Add project files.

---
+++ Telnet/Server/Controller/TiendaController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Telnet.Shared.Models;
+
+namespace Telnet.Server.Controller
+{
+    [Route("api/tienda")]
+    public class TiendaController : ControllerBase
+    {
+        
+        private HttpClient _httpClient;
+        public TiendaController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        [Route("listartiendas/")]
+        public async Task<Tienda> ObtenerTiendas()
+        {
+            Tienda tienda = new Tienda();
+            HttpResponseMessage httpResponseMessage = await _httpClient.GetAsync("https://localhost:7036/servicios/Tienda");
+            httpResponseMessage.EnsureSuccessStatusCode();
+            string responseBody = await httpResponseMessage.Content.ReadAsStringAsync();
+            tienda = JsonConvert.DeserializeObject<Tienda>(responseBody);
+            return tienda;
+        }
+
+    }
+}
---
+++ Telnet/Shared/Models/Tienda.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telnet.Shared.Models
+{
+    public class Tienda
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+    }
+}
